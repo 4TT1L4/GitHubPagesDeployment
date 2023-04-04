e79b96c18f6969195ef310451ab4a17c3df808c2
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2023-04-04 13:58:12 +0200

    Added branch information to the generated static content

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 GIT_REVISION=$(git rev-parse --verify HEAD)
-BUILD_TIME="$(date --iso-8601=seconds --utc)"
+BUILD_TIME=$(date --iso-8601=seconds --utc)
+BRANCH=$(git branch --show-current)
 
 echo "GIT_REVISION: $GIT_REVISION"
 echo "BUILD_TIME: $BUILD_TIME"
+echo "BRANCH: $BRANCH"
 
 rm -rf ./generated_docs
 mkdir ./generated_docs
@@ -12,6 +14,7 @@ cp ./template.html ./generated_docs/index.html
 
 sed -i "s/GIT_REVISION/${GIT_REVISION}/g" ./generated_docs/index.html
 sed -i "s/BUILD_TIME/${BUILD_TIME}/g" ./generated_docs/index.html
+sed -i "s/BRANCH/${BRANCH}/g" ./generated_docs/index.html
 
 echo "Result:"
 cat ./generated_docs/index.html
