f7a6dffc80fbe398f0f89ed157e2bb89f77305a5
4TT1L4 <2914096+4TT1L4@users.noreply.github.com>
2023-04-04 13:21:02 +0200

    Initial commit

---
+++ generate-docs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GIT_REVISION=$(git rev-parse --verify HEAD)
+BUILD_TIME="$(date --iso-8601=seconds --utc)"
+
+echo "GIT_REVISION: $GIT_REVISION"
+echo "BUILD_TIME: $BUILD_TIME"
+
+rm -rf ./generated_docs
+mkdir ./generated_docs
+cp ./index.html ./generated_docs
+
+sed -i "s/GIT_REVISION/${GIT_REVISION}/g" ./generated_docs/index.html
+sed -i "s/BUILD_TIME/${BUILD_TIME}/g" ./generated_docs/index.html
+
+echo "Result:"
+cat ./generated_docs/index.html
+
+echo "== FINISHED =="
